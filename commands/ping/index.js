25b27686b60ad4e56d25d5ed1e213b5c75fa329a
Kenichii <ana.kenichii@gmail.com>
2020-03-03 20:01:28 +0800

    Changed Botchog's reply in ping

--- commands/ping/index.js
+++ commands/ping/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'ping',
   description: 'Returns the amount of time it took for the message to get to Botchog\'s hands',
-  execute(msg, args) {
-    msg.reply(`Pong! It took ${msg.client.ws.ping} ms to get to you`)
+  execute(msg) {
+    msg.reply(`Pong! It took ${msg.client.ws.ping} ms to get to you, love.`)
   },
 }
