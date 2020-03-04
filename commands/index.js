492e7832cf6eebb1bcab81d9391106ae7a5debc5
Kenichii <ana.kenichii@gmail.com>
2020-03-04 10:55:30 +0800

    Added .n command for Botchog\'s one-liner joke generator.

--- commands/index.js
+++ commands/index.js
@@ -3,4 +3,5 @@ module.exports = {
   Self: require('./self'),
   Avatar: require('./avatar'),
   R6: require('./r6'),
+  N: require('./n'),
 };
---
+++ commands/n/index.js
@@ -0,0 +1,12 @@
+const joker = require('one-liner-joke')
+
+module.exports = {
+  name: 'n',
+  description: 'A one-liner joke generator.',
+  execute(msg) {
+    let joke = joker.getRandomJoke({
+      'exclude_tags': ['racist']
+    });
+    msg.reply(joke.body)
+  }
+}
